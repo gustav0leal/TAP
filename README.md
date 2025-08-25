2db1d8a77b82848e2eac143bc474253d47c35882
gustav0leal <gustavoleal@discente.ufg.br>
2025-08-24 21:44:23 -0300

    adding segtree

---
+++ README.md
@@ -0,0 +1,2 @@
+# Topicos Avançados de Programação
+Estarão listados aqui os codigos apresentados durante a disciplina
---
+++ seg/seg.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lef(x) (x<<1) 
+#define rig(x) (lef(x)|1) 
+typedef long long ll;
+const int N = 200'010;
+ll v[N],tree[N<<2];
+const ll v_null = 0;
+ll f(ll a, ll b)
+{
+    return a+b;
+}
+
+void update_tree(int tree_id, int tree_l, int tree_r, int upd_id, ll upd_v)
+{
+    if(tree_l == tree_r)
+    {
+        tree[tree_id] = upd_v;
+        return;
+    }
+    int tmid = (tree_l+tree_r)>>1;
+    // lef(tree_id) tree_l tmid
+    // rig(tree_id) tmid+1 tree_r
+    if(upd_id <= tmid)
+        update_tree(lef(tree_id),tree_l,tmid,upd_id,upd_v);
+    else
+        update_tree(rig(tree_id),tmid+1,tree_r,upd_id,upd_v);
+    tree[tree_id] = f(tree[lef(tree_id)],tree[rig(tree_id)]);
+}
+
+ll query_tree(int tree_id, int tree_l, int tree_r, int v_l, int v_r)
+{
+    if(v_l > v_r)
+        return v_null;
+    if(tree_l == v_l && tree_r == v_r)
+        return tree[tree_id];
+
+    int tmid = (tree_l + tree_r) >> 1;
+    return f(
+        query_tree(lef(tree_id), tree_l, tmid, v_l, min(tmid,v_r)),
+        query_tree(rig(tree_id), tmid+1, tree_r, max(tmid+1,v_l), v_r)
+    );
+}
+
+void build(int tree_id, int tree_l, int tree_r)
+{
+    if(tree_l == tree_r){
+        tree[tree_id] = v[tree_l];
+    }else
+    {
+        int tmid = (tree_l + tree_r) >> 1;
+        build(lef(tree_id), tree_l, tmid);
+        build(rig(tree_id), tmid+1, tree_r);
+        tree[tree_id] = f(tree[lef(tree_id)],tree[rig(tree_id)]);
+    }
+}
+
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    for(int i = 1; i <= n; i++)
+        cin >> v[i];
+    build(1,1,n);
+    while(q--)
+    {
+        int tipo;
+        cin >> tipo;
+        if(tipo == 1)
+        {
+            int id;
+            ll v;
+            cin >> id >> v;
+            update_tree(1, 1, n, id, v);
+        }else
+        {
+            int l, r;
+            cin >> l >> r;
+            cout << query_tree(1, 1, n, l ,r) << '\n';
+        }
+    }
+
+}
